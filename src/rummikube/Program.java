bf9afa31f95ff2a9d4f96efe2923e027a224ca53
Arthur <Arthur@ArthurPC>
2015-12-05 23:26:13 +0200

    Merge origin/master
    most changes are cosmetic

--- src/rummikube/Program.java
+++ src/rummikube/Program.java
@@ -1,18 +1,15 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This is the main class that luches the aplication
  */
 package rummikube;
 
 import static javafx.application.Application.launch;
 
-/**
- *
- * @author giladPe
- */
+
 public class Program {
     public static void main(String[] args) {
         launch(args);
     }
 }
+
+//gilad ata roe et ze???
