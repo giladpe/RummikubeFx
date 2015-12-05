ffdf6d07c110ffd7b8447da70fee3dd28f83e4fb
Arthur <Arthur@ArthurPC>
2015-12-05 23:32:14 +0200

    Merge origin/master
    most changes are cosmetic

--- src/rummikube/Rummikube.java
+++ src/rummikube/Rummikube.java
@@ -14,6 +14,7 @@ import javafx.stage.Stage;
  */
 public class Rummikube extends Application {
   
+    @Override
     public void start(Stage primaryStage)throws Exception{  
         primaryStage.setTitle("Rummikube");
         URL url = getClass().getResource("view/MainMenu.fxml");
