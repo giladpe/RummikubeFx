58b71169af3dbf37a3b6dc172b97c56e867afcb3
Arthur <Arthur@ArthurPC>
2015-12-06 20:07:55 +0200

    Merge origin/master
    most changes are cosmetic

--- src/rummikube/view/GameParametersController.java
+++ src/rummikube/view/GameParametersController.java
@@ -8,6 +8,7 @@ package rummikube.view;
 
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.ResourceBundle;
 import static javafx.collections.FXCollections.observableArrayList;
 import javafx.event.ActionEvent;
@@ -15,6 +16,7 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
@@ -25,21 +27,15 @@ import javafx.scene.layout.HBox;
  * @author giladPe
  */
 public class GameParametersController implements Initializable {
-
-    @FXML
-    ChoiceBox<Integer> numberOfPlayers;
-    @FXML
-    TextField gameName;
-    @FXML
-    HBox PlayerName1;
-    @FXML
-    HBox PlayerName2;
-    @FXML
-    HBox PlayerName3;
-    @FXML
-    HBox PlayerName4;
-
-    @FXML
+    private static final String EMPTY_STRING = "";
+    
+    @FXML ChoiceBox<Integer> numberOfPlayers;
+    @FXML TextField gameName;
+    @FXML HBox PlayerName1;
+    @FXML HBox PlayerName2;
+    @FXML HBox PlayerName3;
+    @FXML HBox PlayerName4;
+    
     ArrayList<HBox> players;
 
     /**
@@ -56,7 +52,6 @@ public class GameParametersController implements Initializable {
         players.add(PlayerName2);
         players.add(PlayerName3);
         players.add(PlayerName4);
-
     }
 
     @FXML
@@ -77,38 +72,101 @@ public class GameParametersController implements Initializable {
             });
         }
     }
+    
+    @FXML
+    protected void handlePlayerNameTextChange(ActionEvent event) {
+        final String gameNameString = gameName.getText();
 
-//    private void setNumberOfPlayersButtonEnabled() {
-//        numberOfPlayers.setDisable(false);
-//    }
+        //* starts with dont works
+        if (!(gameNameString.isEmpty() || gameNameString.startsWith("\\s" ))) {
+            if (true/*|| check if not exsists*/) {
+                
+            }
+        }
+    }
+
     @FXML
     protected void handleBackToMenuButtonAction(ActionEvent event) {
 
     }
+    
+    @FXML
+    protected void handleStartPlayingButtonAction(ActionEvent event) {
+
+    }
+    
+    @FXML
+    protected void handleCheckBoxSelection(ActionEvent event) {
+        int index =  players.indexOf(((Node)event.getSource()).getParent());
+        if (((CheckBox)event.getSource()).isSelected()) {
+            players.get(index).getChildren().stream().forEach((hNode) -> {
+               if (hNode.getClass() == TextField.class) {
+                   ((TextField)hNode).setText(EMPTY_STRING);
+                   ((TextField)hNode).setDisable(true);
+               } });           
+        }
+        else {
+            players.get(index).getChildren().stream().forEach((hNode) -> {
+            if (hNode.getClass() == TextField.class) {
+                ((TextField)hNode).setDisable(false);
+            } });
+        }
+
+    }
+    
+    
 
     @FXML
     private void handleNumberOfPlayersChoiceChange(ActionEvent event) {
         hidePlayersFields();
-        int i;
-        for (i = 0; i < this.numberOfPlayers.getValue(); i++) {
-            this.players.get(i).setVisible(true);
-        }
-        for(;i<4;i++){
-            for (Node hNode : this.players.get(i).getChildren()) {
-                if(hNode.getId().equals("name")){
-                    
-                }
-            }
-                  
-        }
-    }
-        @FXML
-        private void hidePlayersFields
+        Iterator<HBox> iterator = players.iterator();
+        int i = 0;
         
-            () {
-        for (int i = 0; i < 4; i++) {
-                this.players.get(i).setVisible(false);
-            }
+        while (i < this.numberOfPlayers.getValue() && iterator.hasNext()) {
+            HBox currPlayer = iterator.next();
+            currPlayer.setVisible(true);
+            i++;
         }
-
+        
+        while (iterator.hasNext()){
+            HBox currPlayer = iterator.next();
+            currPlayer.getChildren().stream().forEach((hNode) -> {
+                if (hNode.getClass() == TextField.class) {
+                    ((TextField)hNode).setText(EMPTY_STRING);
+                }
+                else if (hNode.getClass() == CheckBox.class) {
+                    ((CheckBox)hNode).setSelected(false);
+                } 
+            });
+            
+//            for (Node hNode : currPlayer.getChildren()) {
+//                if (hNode.getClass() == TextField.class) {
+//                    ((TextField)hNode).setText(EMPTY_STRING);
+//                }
+//                else if (hNode.getClass() == CheckBox.class) {
+//                    ((CheckBox)hNode).setSelected(false);
+//                }
+//            }
+        }
+        
+//        int i;
+//        for (i = 0; i < this.numberOfPlayers.getValue(); i++) {
+//            this.players.get(i).setVisible(true);
+//        }
+//        
+//        for(;i<4;i++){
+//            for (Node hNode : this.players.get(i).getChildren()) {
+//                if(hNode.getId().equals("name")){
+//                    
+//                }
+//            }
+//                  
+//        }
     }
+    
+    @FXML
+    private void hidePlayersFields() {
+        players.stream().forEach((player) -> { player.setVisible(false); });
+    }
+
+}
